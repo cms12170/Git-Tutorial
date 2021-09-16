5035227bf4bd3d1221013123bcad11be69899599
cms12170 <90140306+cms12170@users.noreply.github.com>
2021-09-17 00:23:08 +0900

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -3,7 +3,7 @@
 int main()
 {
 	int a = 0, b = 0;
-	printf("두 정수를  : ");
+	printf("두 정수를 입력하세요  : "); 
 	scanf_s("%d %d", &a, &b);
 	printf("%d & %d = %d\n", a, b, a & b);
 	printf("%d | %d = %d\n", a, b, a | b);
