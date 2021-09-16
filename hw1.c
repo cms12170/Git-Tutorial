37408e09a1697e8a7ced570ec45a6a859bc7e753
cms12170 <90140306+cms12170@users.noreply.github.com>
2021-09-17 00:16:31 +0900

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 0, b = 0;
+	printf("두 정수를 입력하시오 : ");
+	scanf_s("%d %d", &a, &b);
+	printf("%d & %d = %d\n", a, b, a & b);
+	printf("%d | %d = %d\n", a, b, a | b);
+	printf("%d ^ %d = %d\n", a, b, a ^ b);
+	return 0;
+}
