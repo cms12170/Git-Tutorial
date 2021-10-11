32ea74637af9f1cefc031ac132d4e73a90eb36fb
cms12170 <90140306+cms12170@users.noreply.github.com>
2021-10-11 20:09:30 +0900

    Add files via upload

---
+++ hw3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i, cnt = 0;
+
+    printf("Please enter a number : ");
+    scanf_s("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+            cnt++;
+    }
+
+    if (cnt == 2)
+        printf("It is a prime number");
+    else
+        printf("It is not a prime number");
+
+    return 0;
+}
